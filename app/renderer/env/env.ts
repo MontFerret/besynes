a461ae9c46f19add128cb34c6f5d5017e6aef9fa
Tim Voronov <ziflex@gmail.com>
2019-09-08 12:11:43 -0400

    it goes nowhere

--- app/renderer/env/env.ts
+++
@@ -1,5 +0,0 @@
-import { Api } from '../api/api';
-
-export interface AppEnv {
-    api: Api;
-}
--- app/renderer/index.ts
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './app';
-import './index.css';
-
-const app = new App();
-
-ReactDOM.render(app.createElement(), document.getElementById('app'));
